3e27feb7851cf21aa4aedb9f7283218b7b8bfaa8
MiguelOsio800 <miguelosio80@gmail.com>
2025-01-20 14:10:17 -0400

    CHANGE: SE ACTUALIZO EL SERIALIZER DE TASKS

--- helpdesk/tasks/serializers.py
+++ helpdesk/tasks/serializers.py
@@ -1,13 +1,9 @@
-##############################  
-##### Serializers para Tareas ## 
-##############################  
-
 from rest_framework import serializers  # Importamos el módulo serializers de Django REST Framework  
 from .models import Task, Prioridad, Status, Clasificacion  # Importamos los modelos necesarios  
 from usuarios.models import Usuario  # Importamos el modelo Usuario  
 
 ##################################  
-##### Clase PrioridadSerializer ## 
+##### Clase PrioridadSerializer ##   
 ##################################  
 class PrioridadSerializer(serializers.ModelSerializer):  
     class Meta:  
@@ -15,7 +11,7 @@ class PrioridadSerializer(serializers.ModelSerializer):
         fields = '__all__'  # Incluimos todos los campos del modelo  
 
 ##################################  
-##### Clase StatusSerializer ##### 
+##### Clase StatusSerializer #####   
 ##################################  
 class StatusSerializer(serializers.ModelSerializer):  
     class Meta:  
@@ -23,7 +19,7 @@ class StatusSerializer(serializers.ModelSerializer):
         fields = '__all__'  # Incluimos todos los campos del modelo  
 
 ##########################################  
-##### Clase ClasificacionSerializer ######
+##### Clase ClasificacionSerializer ######  
 ##########################################  
 class ClasificacionSerializer(serializers.ModelSerializer):  
     class Meta:  
@@ -35,12 +31,16 @@ class ClasificacionSerializer(serializers.ModelSerializer):
 ##################################  
 
 class TaskSerializer(serializers.ModelSerializer):  
-    area_nombre = serializers.CharField(source='area.nombre', read_only=True)  # Campo solo de lectura para el nombre del área  
-    usuario_nombre = serializers.CharField(source='usuario.first_name', read_only=True)  # Campo solo de lectura para el nombre del usuario  
-    status_nombre = serializers.CharField(source='status.estado', read_only=True)  # Campo solo de lectura para el estado  
-    prioridad_nombre = serializers.CharField(source='prioridad.nivel', read_only=True)  # Campo solo de lectura para el nivel de prioridad  
-    clasificacion_tema = serializers.CharField(source='clasificacion.tema', read_only=True)  # Campo solo de lectura para el tema de clasificación  
-    tecnicos_nombres = serializers.SerializerMethodField()  # Campo personalizado para obtener los nombres de técnicos  
+    area_nombre = serializers.CharField(source='area.nombre', read_only=True)  
+    usuario_nombre = serializers.CharField(source='usuario.first_name', read_only=True)  
+    status_nombre = serializers.CharField(source='status.estado', read_only=True)  
+    prioridad_nombre = serializers.CharField(source='prioridad.nivel', read_only=True)  
+    clasificacion_tema = serializers.CharField(source='clasificacion.tema', read_only=True)  
+    tecnicos_nombres = serializers.SerializerMethodField()  
+
+    # Se añaden los campos para permisos de escritura  
+    tecnicos = serializers.PrimaryKeyRelatedField(many=True, queryset=Usuario.objects.all(), required=False)  
+    prioridad = serializers.PrimaryKeyRelatedField(queryset=Prioridad.objects.all(), required=False)  
 
     class Meta:  
         model = Task  # Modelo que se va a serializar  
@@ -58,12 +58,28 @@ class TaskSerializer(serializers.ModelSerializer):
             'status_nombre',   
             'prioridad',   
             'prioridad_nombre',  
-            'fecha_creacion',  # Incluir fecha de creación  
-            'fecha_final',  # Incluir fecha final  
+            'fecha_creacion',  
+            'fecha_final',  
             'clasificacion',  
             'clasificacion_tema'  
         ]  
 
     def get_tecnicos_nombres(self, obj):  
-        # Método para obtener los nombres completos de los técnicos  
-        return [f"{tecnico.first_name} {tecnico.last_name}" for tecnico in obj.tecnicos.all()]
+        return [f"{tecnico.first_name} {tecnico.last_name}" for tecnico in obj.tecnicos.all()]  
+
+    def update(self, instance, validated_data):  
+        # Actualizamos los técnicos  
+        tecnicos = validated_data.pop('tecnicos', None)  
+        if tecnicos is not None:  
+            instance.tecnicos.set(tecnicos)  # Establece la relación con los nuevos técnicos  
+
+        # Actualizar la prioridad si se proporciona  
+        prioridad = validated_data.pop('prioridad', None)  
+        if prioridad is not None:  
+            instance.prioridad = prioridad  
+
+        # Actualizamos los otros campos del modelo  
+        for attr, value in validated_data.items():  
+            setattr(instance, attr, value)  
+        instance.save()  
+        return instance
